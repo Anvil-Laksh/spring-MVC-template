60505312c138586ecb6fe77f3a7431821f6bc5c1
b-Laksh <60606657+b-Laksh@users.noreply.github.com>
2020-03-01 12:46:06 +0530

    first commit

--- src/main/java/com/example/demo/controller/TestControllers.java
+++ src/main/java/com/example/demo/controller/TestControllers.java
@@ -2,15 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.Customer;
 import com.example.demo.service.TestService;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController("/edu")
 @Slf4j
